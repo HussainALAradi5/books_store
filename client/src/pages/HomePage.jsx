4df8414bab5ad7369870544ea1b7212edd0d4b20
HussainALAradi5 <hussainaradi.ha@gmail.com>
2024-07-28 21:29:50 +0300

    enhance the visual of the homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import BookCard from "../components/BookCard";
+
 const HomePage = () => {
   const [books, setBooks] = useState([]);
 
@@ -21,13 +22,15 @@ const HomePage = () => {
   return (
     <div className="homePage">
       <h1>All Books</h1>
-      <div className="bookGrid">
+      <ul className="bookGrid">
         {books.map((book) => (
-          <Link to={`/books/${book._id}`} key={book._id}>
-            <BookCard book={book} />
-          </Link>
+          <li key={book._id}>
+            <Link to={`/books/${book._id}`} className="bookLink">
+              <BookCard book={book} />
+            </Link>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 };
